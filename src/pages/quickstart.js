652c2e304a32d8a32a3311d4d95521903da66dba
Ryunosuke Onda <60596905+ryu-liam@users.noreply.github.com>
2024-01-31 19:53:06 +0900

    Add Link component and create QuickStart page

---
+++ src/pages/quickstart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function QuickStartPage() {
+  return (
+    <div>
+      <h1>Quick Start</h1>
+      <p>
+        This is a quick start page. You can edit it at{" "}
+        <code>src/pages/quickstart.js</code>.
+      </p>
+    </div>
+  );
+}
